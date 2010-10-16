132da1bbd3320c2b19bc28d85ba857cd5248d2c7
Jason Rohwedder <jro@badgerhound.com>
2010-10-16 16:10:59 -0500

    fix typo in migration

--- db/migrate/20101016183320_add_more_basic_attributes_to_videos.rb
+++ db/migrate/20101016183320_add_more_basic_attributes_to_videos.rb
@@ -1,7 +1,7 @@
 class AddMoreBasicAttributesToVideos < ActiveRecord::Migration
   def self.up
     add_column :videos, :thumbnail, :string
-    add_column :videos, :featured, :bool
+    add_column :videos, :featured, :boolean
     add_column :videos, :external_rating, :int
     add_column :videos, :description, :text
   end
