82186787e6ba094aaa73c2fe8b2dc0cf564a4b24
Michael Sepcot <michael@viewpoints.com>
2010-10-15 19:16:23 -0500

    empty rails project with jquery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieNight::Application.config.secret_token = '7f6526ee0ef02cda9e2f64af30d007159556777da56194d31c37b8a57140ae4fb84651456c315a3c1adb1ec2955f0736ef16dc898ad773dbddf9c27d47f955cb'
