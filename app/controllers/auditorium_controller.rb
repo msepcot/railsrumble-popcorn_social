ceb33173ad84a4353ab574bff3d4d05d37afd90f
Michael Sepcot <michael@viewpoints.com>
2010-10-15 23:48:11 -0500

    placeholder graphics/stylesheet

--- app/controllers/auditorium_controller.rb
+++ app/controllers/auditorium_controller.rb
@@ -1,5 +1,6 @@
 class AuditoriumController < ApplicationController
   def index
     @video = OEmbed::Providers.get('http://www.hulu.com/watch/169366/black-sheep')
+    # Pusher['black-sheep'].trigger('chatroom',{:text => 'pusher!'})
   end
 end
